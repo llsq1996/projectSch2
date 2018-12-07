4456e4c243fa35c41b9be2aec9990f57a8bbf622
Halcyone1997 <939861903@qq.com>
2018-12-07 17:35:44 +0800

    省市区查询

---
+++ src/main/java/com/example/demo/entity/exEntity/ShopRec.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity.exEntity;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.util.List;
+@Data
+public class ShopRec {
+    private String spName;
+    private List<String> addressList;
+    private String leader;
+    private String leaderPhone;
+    private String cusPhone;
+    private Integer category;
+}
